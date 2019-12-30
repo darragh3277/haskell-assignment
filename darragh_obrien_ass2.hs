7b1e2bc54676b4744020bd8bb80b663f1c2ba473
Darragh <darragh3277@gmail.com>
2019-12-30 19:33:50 +0000

    question 5 done

--- darragh_obrien_ass2.hs
+++ darragh_obrien_ass2.hs
@@ -14,14 +14,18 @@ remove x list = [y | y <- list, y /= x]
 
 --Question 1
 --is_square :: Int -> Bool
-is_square x
-  | x < 1       = False
-  | x == 1      = True
-  | otherwise   = is_square (x `div` 2)
+--4 is the lowest square, so start at 2. increment from 2 to input squaring each int. if int 
+--equals the input is a square. once it goes above the input it's not a square. Not super efficient but gets the job done
+is_square :: Int -> Bool
+is_square x = chk 2 where
+  chk n | n * n < x = chk (n + 1)
+        | n > x = False
+        | n * n == x = True
+        | otherwise = False
   
 --Question 2
--- remove spaces from string and convert to lower case first
--- iterate through unique elements, count occurences and divide by lenth of string
+--remove spaces from string and convert to lower case first
+--iterate through unique elements, count occurences and divide by lenth of string
 freq_letter_pc :: String -> [(Float, Char)]
 freq_letter_pc list = let new_list = map toLower (remove ' ' list)
                       in [(fromIntegral (num_occ y new_list) / fromIntegral (length new_list), y) | y <- nub new_list, elem y ['a'..'z'] ]
@@ -31,16 +35,18 @@ freq_letter_pc list = let new_list = map toLower (remove ' ' list)
 cities=[(1,"Paris",7000000,1),(2,"London",8000000,2),(1,"Rome",3000000,3),(1,"Edinburgh",500000,2),(1,"Florence",50000,3),(1,"Venice",200000,3), (1,"Lyon",1000000,1),(1,"Milan",3000000,3),(1,"Madrid",6000000,4),(1,"Barcelona",5000000,4)]
 --id, country
 countries=[(1,"UK"),(2,"France"),(3,"Italy"),(4,"Spain")]
--- 3.A
+--3.A
 get_city_above :: Int -> [String]
 get_city_above x = [y | (_,y,z,_) <- cities, z >= x]
--- 3.B
+--3.B
 get_city :: String -> [String]
 get_city x = [city | (id, country) <- countries, (_, city, _, country_id) <- cities, x == country && id == country_id]
--- 3.C
+--3.C
 num_city :: [(String, Int)]
 num_city = [(country, length (get_city country)) | (_, country) <- countries]
 
 --Question 4
 eucl_dist :: [Float] -> [Float] -> Float
 eucl_dist xs ys = sqrt (sum [(x - y) ^ 2 | (x,y) <- zip xs ys])
+
+--Question 5
